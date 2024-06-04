b348cd899cc98050c673755e4aac924071f9c86f
deepa-ev-dev <deepavenkataramanaiah@gmail.com>
2024-06-04 18:01:29 +0530

    Removed old files from repository, added new files

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBYNnYkffgloXyjSwQyQH5mYZmLzErQO_s",
-  authDomain: "netflixgpt-35f55.firebaseapp.com",
-  projectId: "netflixgpt-35f55",
-  storageBucket: "netflixgpt-35f55.appspot.com",
-  messagingSenderId: "992136001857",
-  appId: "1:992136001857:web:b165492493cb08d3c609c0",
-  measurementId: "G-4V3NVP0WD3"
+  apiKey: "AIzaSyBIh5kFVtPhsZ3IZi2kddwCv86r7ZsXheM",
+  authDomain: "netflix-gpt-deepa.firebaseapp.com",
+  projectId: "netflix-gpt-deepa",
+  storageBucket: "netflix-gpt-deepa.appspot.com",
+  messagingSenderId: "927973523506",
+  appId: "1:927973523506:web:8cb9d3744f55819f5abc2b",
+  measurementId: "G-65533CCX02"
 };
 
 // Initialize Firebase
