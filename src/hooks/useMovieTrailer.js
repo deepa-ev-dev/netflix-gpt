7c1318a68a07889b9fc0006eacdf6fa9280599f0
deepa-ev-dev <deepavenkataramanaiah@gmail.com>
2024-06-11 14:39:45 +0530

    Memoization done

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,19 +1,25 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addTrailerVideo } from "../utils/movieSlice";
 const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch();
+
+  const trailerVideo= useSelector((store) => store.movies.trailerVideo);
   useEffect(() => {
     const getMovieVideos = async () => {
       try {
         const data = await fetch(
-          `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+          "https://api.themoviedb.org/3/movie/" +
+            movieId +
+            "/videos?language=en-US",
           API_OPTIONS
         );
         const json = await data.json();
 
-        const filterData = json.results.filter((video) => video.type === "Trailer");
+        const filterData = json.results.filter(
+          (video) => video.type === "Trailer"
+        );
         const trailer = filterData.length ? filterData[0] : json.results[0];
 
         dispatch(addTrailerVideo(trailer));
@@ -23,7 +29,7 @@ const useMovieTrailer = (movieId) => {
       }
     };
 
-    getMovieVideos();
+    !trailerVideo && getMovieVideos();
 
     return () => {
       // Cleanup function to cancel any pending requests or unsubscribe
